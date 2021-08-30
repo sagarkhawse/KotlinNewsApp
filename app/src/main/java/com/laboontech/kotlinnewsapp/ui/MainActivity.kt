d7d38f5ef65f4fd39d9d14de5236bc1979ce593f
Sagarkhawse <sagar.khawse@gmail.com>
2021-08-30 16:17:04 +0530

    Initial commit

---
+++ app/src/main/java/com/laboontech/kotlinnewsapp/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.laboontech.kotlinnewsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.laboontech.kotlinnewsapp.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+    }
+}
---
+++ app/src/main/java/com/laboontech/kotlinnewsapp/ui/fragments/SavedFragment.kt
@@ -0,0 +1,7 @@
+package com.laboontech.kotlinnewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.laboontech.kotlinnewsapp.R
+
+class SavedFragment : Fragment(R.layout.fragment_saved) {
+}
